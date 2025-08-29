7858d315c6f7cc8299482751256bab6de8791610
Guto Okada <gutookada@gmail.com>
2025-08-29 09:38:35 +0000

    deixe com 800 pixels

--- src/app/dashboard/annotations/page.tsx
+++ src/app/dashboard/annotations/page.tsx
@@ -268,7 +268,7 @@ export default function AnnotationsPage() {
                     </AlertDialog>
                 )}
             </div>
-            <Separator className="w-[865px] mx-auto" />
+            <Separator className="w-[800px] mx-auto" />
           </CardHeader>
           <CardContent>
             <Table>
